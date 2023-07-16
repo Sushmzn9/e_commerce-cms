b4c153b7a38b946de7a06818be0ac2421ec1757d
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-07-16 14:19:23 +1000

    project initiaized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Signup } from "./components/pages/Signin-signup/Signup";
+import { Signin } from "./components/pages/Signin-signup/Signin";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        {/* Private Components */}
+        <Route path="/new-admin" element={<Signup />} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
